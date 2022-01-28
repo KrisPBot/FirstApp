53acd7724725eebe025261cfc2197fdab4096187
Kris Penn <kristophermp2001@gmail.com>
2022-01-28 15:17:34 -0500

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Kris Penn on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
